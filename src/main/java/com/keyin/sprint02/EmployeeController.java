a9ac0ae0dde672772353123b15134a66287b3d8a
Matt Murphy <45915909+Mattmurphy97@users.noreply.github.com>
2022-12-10 00:55:01 -0330

    updated annotation in backend for react project

--- src/main/java/com/keyin/sprint02/EmployeeController.java
+++ src/main/java/com/keyin/sprint02/EmployeeController.java
@@ -1,11 +1,15 @@
 package com.keyin.sprint02;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+// @CrossOrigin allows the backend to connect with the front end when given the "localhost" port#,
+// in this case it is 3000 (react app)
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 //@RequestMapping("/api")
 public class EmployeeController {
