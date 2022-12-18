e265336efb09640ab74ba8224f541f6c02a6a65f
Matt Murphy <45915909+Mattmurphy97@users.noreply.github.com>
2022-12-18 13:01:43 -0330

    Made a major error, had to restore!

---
+++ src/main/java/com/keyin/sprint02/Sprint02Application.java
@@ -0,0 +1,13 @@
+package com.keyin.sprint02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sprint02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sprint02Application.class, args);
+	}
+
+}
